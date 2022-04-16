1004b6d7e04b73bc051751e74ceff9c4c6c7516e
Brianna Baker <brirose.baker@gmail.com>
2022-04-16 13:23:02 -0700

    made profit model and did informal sensitivity analysis

--- R/almond_profit.R
+++ R/almond_profit.R
@@ -4,7 +4,7 @@
 #' Almond Yield Anomaly Model
 #' Function to calculate the yield (tons/acre) of perennial crops
 #' takes coefficient inputs from Lobell et al. 2006 (crop growth) and climate data
-#' outputs yield anomaly for each year of climate data
+#' outputs profit from almond sales for each year of climate data
 #' note that default values are for almonds, would need to add to model for crops that include max temps as predictor
 #'
 #' @param climate_data dataframe, climate data input with temperature (C) and precipitation (mm) 
@@ -15,16 +15,21 @@
 #' @param c third coefficient (precipitation; per Lobell, 2006)
 #' @param d fourth coefficient (precipitation; per Lobell, 2006)
 #' @param intercept intercept (per Lobell, 2006)
+#' @param avg_yield average yield per year in tons
+#' @param avg_cost average cost in dollars per ton
+#' @param acres_used acres of crop
+#' 
 #'
-#' @return 
+#' @return profit form almond sales for each year of climate data ($)
 #' @export
 #'
 #' @examples
-almond_yield_anom = function(climate_data, temp_min_month = 2,
-                             precip_month = 1, 
+almond_profit = function(climate_data, temp_min_month = 2, precip_month = 1, 
                              a = -0.015, b = -0.0046, 
-                             c = -0.07, d = 0.0043, intercept = 0.28) {
-
+                             c = -0.07, d = 0.0043, intercept = 0.28,
+                             avg_yield = 1500000, avg_cost = 4000, 
+                             acres_used = 1600000) {
+  
   # manipulation of inputs to create a dataframe of minimum temperatures
   tn_2_values <- climate_data %>% 
     filter(month == temp_min_month) %>% 
@@ -41,18 +46,32 @@ almond_yield_anom = function(climate_data, temp_min_month = 2,
   year_range <- seq(from = min(tn_2_values$year), to = max(tn_2_values$year), by = 1)
   
   # create empty vector for model output
-  almond_yield_anom_tons_per_acre <- vector(mode = 'numeric', length = length(year_range))
+  almond_profit <- vector(mode = 'numeric', length = length(year_range))
+  
+  almond_yield_tons_per_acre <- vector(mode = 'numeric', length = length(year_range))
+  
+  almond_profit_anom <- vector(mode = 'numeric', length = length(year_range))
   
   # calculate almond yield anomaly
   
   for (i in 1:length(year_range)) {
     tn_2 <- tn_2_values$tn_2[tn_2_values$year == year_range[i]]
     p_1 <- p_1_values$p_1[p_1_values$year == year_range[i]]
-    anom_calc <- (a * tn_2) + (b * tn_2 ^ 2) + (c * p_1) + (d * p_1 ^ 2) + intercept
-    almond_yield_anom_tons_per_acre[i] <- anom_calc
+    anom_calc_tons_per_acre <- (a * tn_2) + (b * tn_2 ^ 2) + (c * p_1) + (d * p_1 ^ 2) + intercept
+    yield <- anom_calc_tons_per_acre + avg_yield 
+    profit_anom_calc <- anom_calc_tons_per_acre * avg_cost * acres_used
+    profit_calc <- (yield) * avg_cost * acres_used
+    
+    
+    almond_profit[i] <- profit_calc
+    almond_yield_tons_per_acre[i] <- yield
+    almond_profit_anom[i] <- profit_anom_calc
+
   }
   # create dataframe of model output
-  results <- data.frame(year_range, almond_yield_anom_tons_per_acre, tn_2_values$tn_2, p_1_values$p_1) %>% 
+  results <- data.frame(year_range, almond_profit, almond_profit_anom,
+                        almond_yield_tons_per_acre,
+                        tn_2_values$tn_2, p_1_values$p_1) %>% 
     rename(year = year_range) %>% 
     rename(tn_2_values = tn_2_values.tn_2) %>% 
     rename(p_1_values = p_1_values.p_1)
